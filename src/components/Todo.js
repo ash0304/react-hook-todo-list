ce06eb92d07e7c550b911588bf85613a4156de21
Ash Lai <z0975201508@gmail.com>
2021-08-10 15:00:43 +0800

    use props to pass the lastest list & show the list with todo component

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,5 +1,15 @@
+import React from 'react';
+
 const Todo = (props) => {
-  return <div></div>;
+  const content = props.todos.map((todo, index) => {
+    return (
+      <div key={todo.id} className="todo-row">
+        <div style={{color: 'white'}}>{todo.text}</div>
+      </div>
+    );
+  });
+
+  return <React.Fragment>{content}</React.Fragment>;
 };
 
 export default Todo;
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import Todo from './Todo';
 import TodoForm from './TodoForm';
 
 const TodoList = (props) => {
+  const [todos, setTodos] = useState([]);
+
   const addTodo = (todo) => {
-    console.log(todo);
+    // to prevent add empty string
+    if (!todo.text) return;
+
+    const newTodos = [todo, ...todos];
+    setTodos(newTodos);
   };
 
   return (
     <React.Fragment>
       <h1>What's the Plan for Today?</h1>
       <TodoForm onSubmit={addTodo} />
+      <Todo todos={todos} />
     </React.Fragment>
   );
 };
